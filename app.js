7d7f5ea2cd79ec610c1dc3c7224f63eb09c19a5a
Johnny Ho <johnny5550822@gmail.com>
2013-10-05 00:59:14 -0700

    Get the express package

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+var mysql = require("db-mysql");
 
 var app = express();
 
